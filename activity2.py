f8efb41d6ae4d2335c55ba5d946a97326ebfd753
mustafa276 <mmustafamajeed1@gmail.com>
2024-09-09 11:29:51 +0500

    weather

---
+++ activity2.py
@@ -0,0 +1,6 @@
+def weather_conditions():
+    print("the weather is pleasant in ", spring)
+    print("the weather is the same in ", autumn)
+spring = "spring"
+autumn = "autumn"
+weather_conditions()
